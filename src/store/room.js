37fbe9a86d89a516fb91db836c834074381a49fa
Link1515 <h26281660@gmail.com>
2022-02-16 12:02:46 +0800

    step modal 改放在 start.vue，並改採用vue modal 套件

--- src/store/room.js
+++ src/store/room.js
@@ -1,5 +1,4 @@
 import swal from 'sweetalert2'
-import vm from '../main.js'
 
 export default {
   namespaced: true,
@@ -36,40 +35,6 @@ export default {
     }
   },
   actions: {
-    async SOCKET_runStep ({ state }, gameStep) {
-      const stepList = state.gameInfo.stepList
-      let showPlayers
-
-      switch (stepList[gameStep].mode) {
-        case '語音':
-          await stepVoice(state.msg, stepList[gameStep])
-          break
-        case '顯示':
-          if (stepList[gameStep].data.roleListType === 'all') {
-            showPlayers = state.playerList
-          } else if (stepList[gameStep].data.roleListType === 'funRoleList') {
-            showPlayers = state.playerList.filter(player => player.funRoleId === stepList[gameStep].data.roleId)
-          } else if (stepList[gameStep].data.roleListType === 'goodCampRoleList') {
-            if (stepList[gameStep].data.roleId === 'all') {
-              showPlayers = state.playerList.filter(player => player.camp === true)
-            } else {
-              showPlayers = state.playerList.filter(player => player.campRoleId === stepList[gameStep].data.roleId)
-            }
-          } else if (stepList[gameStep].data.roleListType === 'badCampRoleList') {
-            if (stepList[gameStep].data.roleId === 'all') {
-              showPlayers = state.playerList.filter(player => player.camp === false)
-            } else {
-              showPlayers = state.playerList.filter(player => player.campRoleId === stepList[gameStep].data.roleId)
-            }
-          }
-          await stepShow(showPlayers, stepList[gameStep].data.timer * 1000)
-          break
-        case '標記':
-          await stepMark()
-          break
-      }
-      vm.$socket.emit('stepDone')
-    },
     SOCKET_error (_, msg) {
       swal.fire({
         icon: 'error',
@@ -114,58 +79,4 @@ function getVoices () {
       }, 10)
     }
   )
-}
-
-function stepVoice (msg, step) {
-  return new Promise((resolve, reject) => {
-    msg.onend = resolve
-    msg.text = step.data
-    speechSynthesis.speak(msg)
-  })
-}
-
-function stepShow (showPlayers, timer) {
-  return new Promise((resolve, reject) => {
-    let timerInterval
-    let playersHtml = ''
-
-    showPlayers.map(player => {
-      playersHtml += `<div class="col-3">${player.name}</div>`
-    })
-
-    const html = `
-      <div id="timeBar" style="background: blue; height: 5px"></div>
-      <div class="row flex-wrap justify-content-center" style="width: 100%">${playersHtml}</div>
-    `
-    swal.fire({
-      html,
-      timer,
-      allowOutsideClick: false,
-      showConfirmButton: false,
-      didOpen: () => {
-        const timeBar = swal.getHtmlContainer().querySelector('#timeBar')
-        timerInterval = setInterval(() => {
-          timeBar.style.width = parseInt((swal.getTimerLeft() / timer) * 100) + '%'
-        }, 100)
-      },
-      willClose: () => {
-        clearInterval(timerInterval)
-      }
-    }).then(() => {
-      console.log('顯示成功')
-      resolve()
-    })
-  })
-}
-
-function stepMark () {
-  return new Promise((resolve, reject) => {
-    swal.fire({
-      text: '標記'
-    }).then(() => {
-      vm.$socket.emit('test', 'test')
-      console.log('標記成功')
-      resolve()
-    })
-  })
 }
--- src/views/Room/Start.vue
+++ src/views/Room/Start.vue
@@ -42,7 +42,7 @@
         <TabPanel header="流程控制" v-if="playerData.role === 1">
           <div class="row g-3" style="text-align: center">
             <div class="col-12">
-              <Button label="播放" @click="runStep" icon="pi pi-caret-right" class="p-button-rounded p-button-raised p-button-lg"/>
+              <Button label="播放" @click="startStep" icon="pi pi-caret-right" class="p-button-rounded p-button-raised p-button-lg"/>
             </div>
             <div class="col-12">
               <Button label="重置" @click="resetStep" icon="pi pi-replay" class="p-button-rounded p-button-raised p-button-lg p-button-secondary"/>
@@ -51,6 +51,15 @@
         </TabPanel>
       </TabView>
     </div>
+    <VueModal v-model="stepShowModal" :enableClose="false" :title="currentStepTitle">
+      <div ref="stepShowCountDown" class="mb-5 mt-3" style="background: blue; height: 5px"></div>
+      <div class="row flex-wrap justify-content-center" style="text-align: center">
+        <div class="col-3" v-for="player in showPlayers" :key="player.playerId">
+          <Avatar icon="pi pi-user" class="mr-2" size="large" shape="circle"/>
+          <div>{{ player.name }}</div>
+        </div>
+      </div>
+    </VueModal>
   </div>
 </template>
 
@@ -60,7 +69,9 @@ import Column from 'primevue/column'
 import Avatar from 'primevue/avatar'
 import TabView from 'primevue/tabview'
 import TabPanel from 'primevue/tabpanel'
-
+import VueModal from '@kouts/vue-modal'
+import '@kouts/vue-modal/dist/vue-modal.css'
+import vm from '../../main.js'
 import { mapState, mapGetters } from 'vuex'
 
 export default {
@@ -70,28 +81,74 @@ export default {
     Column,
     Avatar,
     TabView,
-    TabPanel
+    TabPanel,
+    VueModal
+  },
+  data () {
+    return {
+      currentStepTitle: '',
+      stepShowModal: false,
+      showPlayers: []
+    }
   },
   computed: {
-    ...mapState('room', ['gameInfo', 'playerList']),
+    ...mapState('room', ['gameInfo', 'playerList', 'msg']),
     ...mapGetters('room', ['playerData'])
   },
   methods: {
-    runStep () {
-      this.$socket.emit('runStep')
+    startStep () {
+      this.$socket.emit('startStep')
     },
     resetStep () {
       speechSynthesis.cancel()
       this.$socket.emit('resetStep')
     },
-    translateRoleType (RoleType) {
-      switch (RoleType) {
+    stepVoice (msg, step) {
+      return new Promise((resolve, reject) => {
+        msg.onend = resolve
+        msg.text = step.data
+        speechSynthesis.speak(msg)
+      })
+    },
+    stepShow (step, timer) {
+      this.currentStepTitle = `顯示 ${this.translateRoleType(step.data.roleListType)} ${this.translateRoleName(step.data.roleListType, step.data.roleId)}，時間 ${step.data.timer} 秒`
+      const totalTime = timer
+
+      return new Promise((resolve, reject) => {
+        this.stepShowModal = true
+        const intervalTimer = setInterval(() => {
+          timer -= 10
+          this.$refs.stepShowCountDown.style.width = parseInt((timer / totalTime) * 100) + '%'
+
+          if (timer < 0) {
+            clearInterval(intervalTimer)
+            this.stepShowModal = false
+            resolve()
+          }
+        }, 10)
+      })
+    },
+    stepMark () {
+      return new Promise((resolve, reject) => {
+        vm.$swal({
+          text: '標記'
+        }).then(() => {
+          vm.$socket.emit('test', 'test')
+          console.log('標記成功')
+          resolve()
+        })
+      })
+    },
+    translateRoleType (roleType) {
+      switch (roleType) {
         case 'goodCampRoleList':
           return '好人陣營'
         case 'badCampRoleList':
           return '壞人陣營'
         case 'funRoleList':
           return '功能角色'
+        case 'all':
+          return '所有人'
       }
     },
     translateRoleName (roleType, roleId) {
@@ -101,7 +158,47 @@ export default {
             return role.name
           }
         }
+        return '所有人'
       }
+      return ''
+    }
+  },
+  sockets: {
+    async runStep (gameStep) {
+      const stepList = this.gameInfo.stepList
+      const msg = this.msg
+      const playerList = this.playerList
+
+      switch (stepList[gameStep].mode) {
+        case '語音':
+          await this.stepVoice(msg, stepList[gameStep])
+          break
+        case '顯示':
+          if (stepList[gameStep].data.roleListType === 'all') {
+            this.showPlayers = playerList
+          } else if (stepList[gameStep].data.roleListType === 'funRoleList') {
+            this.showPlayers = playerList.filter(player => player.funRoleId === stepList[gameStep].data.roleId)
+          } else if (stepList[gameStep].data.roleListType === 'goodCampRoleList') {
+            if (stepList[gameStep].data.roleId === 'all') {
+              this.showPlayers = playerList.filter(player => player.camp === true)
+            } else {
+              this.showPlayers = playerList.filter(player => player.campRoleId === stepList[gameStep].data.roleId)
+            }
+          } else if (stepList[gameStep].data.roleListType === 'badCampRoleList') {
+            if (stepList[gameStep].data.roleId === 'all') {
+              this.showPlayers = playerList.filter(player => player.camp === false)
+            } else {
+              this.showPlayers = playerList.filter(player => player.campRoleId === stepList[gameStep].data.roleId)
+            }
+          }
+          await this.stepShow(stepList[gameStep], stepList[gameStep].data.timer * 1000)
+          break
+        case '標記':
+          await this.stepMark()
+          break
+      }
+
+      this.$socket.emit('stepDone')
     }
   }
 }
@@ -118,6 +215,15 @@ export default {
   .p-tabview-panels {
     padding: 0;
     padding-top: 2rem;
+  }
+
+}
+
+.vm {
+  top: 50%;
+  transform: translateY(-50%);
+  .vm-titlebar {
+    text-align: center;
   }
 }
 </style>
