f727168ace12ec846818c23772662098324db7c2
Link1515 <88765055+Link1515@users.noreply.github.com>
2022-02-26 13:22:03 +0000

    Deploying to gh-pages from @ Link1515/wda-final-project@928670101af36abe96b8f119cac98eea1dec6296 🚀

---
+++ precache-manifest.7c668612afd0fbd534eda976d5e3ae0b.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f27dcef4606133a7079",
+    "url": "css/CreateGame.0247e792.css"
+  },
+  {
+    "revision": "e230097f7cab4fa8c1d4",
+    "url": "css/Edit.f1ed14a3.css"
+  },
+  {
+    "revision": "b859fbc8d7476fdea2a3",
+    "url": "css/EditInfo.97681093.css"
+  },
+  {
+    "revision": "8633234ac4e01abb4443",
+    "url": "css/MakeGame.632b07e4.css"
+  },
+  {
+    "revision": "9db0209013e083c59e92",
+    "url": "css/ManageGame.5f80d94b.css"
+  },
+  {
+    "revision": "c3551baf3e409ac1cefc",
+    "url": "css/Play.1a2a00b0.css"
+  },
+  {
+    "revision": "6ce92972b00ef2c6a3f0",
+    "url": "css/PopularGame.ba6459c6.css"
+  },
+  {
+    "revision": "e4e09b760af23de7f933",
+    "url": "css/ReportReview.f8eaef5e.css"
+  },
+  {
+    "revision": "1d5b307ba96f285fa661",
+    "url": "css/Room.9512464b.css"
+  },
+  {
+    "revision": "6c79b098ec7ce5c07363",
+    "url": "css/Start.954eae77.css"
+  },
+  {
+    "revision": "ed0bed771ac94382927d",
+    "url": "css/UserCenter.1fe06140.css"
+  },
+  {
+    "revision": "2095d1bc6c742c54e6d3",
+    "url": "css/app.b1abe9f1.css"
+  },
+  {
+    "revision": "761270562d6c4e82597b",
+    "url": "css/chunk-vendors.c8a14855.css"
+  },
+  {
+    "revision": "943c3597cd33be56d53df0d1982fa8ff",
+    "url": "fonts/primeicons.943c3597.woff"
+  },
+  {
+    "revision": "b29a888ff7f07091c7e08eb0d991e221",
+    "url": "fonts/primeicons.b29a888f.ttf"
+  },
+  {
+    "revision": "e01fd4133bac49cd2ea07ad6f7c45695",
+    "url": "fonts/primeicons.e01fd413.eot"
+  },
+  {
+    "revision": "39bba5f853ea64e13aad73573dedd840",
+    "url": "img/banner.39bba5f8.jpg"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "img/color.c7a33805.png"
+  },
+  {
+    "revision": "ea7407d5e12958f7377d8f6eeaa8f301",
+    "url": "img/design.ea7407d5.jpg"
+  },
+  {
+    "revision": "a5b5d4166c094c433226b2d93077cab6",
+    "url": "img/iconfont.a5b5d416.svg"
+  },
+  {
+    "revision": "82a33a43be59e913b58efbdfd64e281e",
+    "url": "img/image-placeholder.82a33a43.png"
+  },
+  {
+    "revision": "0b9ce75fe8c3792d26b5ee2ca71b90d0",
+    "url": "img/loading.0b9ce75f.svg"
+  },
+  {
+    "revision": "c412cabe416215c047aad72df568e6de",
+    "url": "img/logo.c412cabe.png"
+  },
+  {
+    "revision": "04a0c3c38016a8679bcb35f84216c0d8",
+    "url": "img/preparing.04a0c3c3.svg"
+  },
+  {
+    "revision": "64b5d470af63a67aa9b9d3e8a866a35e",
+    "url": "img/primeicons.64b5d470.svg"
+  },
+  {
+    "revision": "0666bd3d1e1ce30c5e65e753f1f0e4eb",
+    "url": "img/trashCan.0666bd3d.png"
+  },
+  {
+    "revision": "50306ce4c321a57812a1d5e697cd13c2",
+    "url": "index.html"
+  },
+  {
+    "revision": "6db424ef55f14d9b2189",
+    "url": "js/BugReport.5308ed10.js"
+  },
+  {
+    "revision": "0f27dcef4606133a7079",
+    "url": "js/CreateGame.dd4e98c9.js"
+  },
+  {
+    "revision": "e230097f7cab4fa8c1d4",
+    "url": "js/Edit.c5ffe682.js"
+  },
+  {
+    "revision": "b859fbc8d7476fdea2a3",
+    "url": "js/EditInfo.dc2e567d.js"
+  },
+  {
+    "revision": "2fe4a94020f632f928d4",
+    "url": "js/EditPassword.e61ee40b.js"
+  },
+  {
+    "revision": "11c86b0e4b103fe5bae4",
+    "url": "js/JoinGame.198f42e3.js"
+  },
+  {
+    "revision": "8633234ac4e01abb4443",
+    "url": "js/MakeGame.d135ad6e.js"
+  },
+  {
+    "revision": "9db0209013e083c59e92",
+    "url": "js/ManageGame.f2b128da.js"
+  },
+  {
+    "revision": "39ee06c5d73a3d39ee98",
+    "url": "js/ManageUser.09044d06.js"
+  },
+  {
+    "revision": "c3551baf3e409ac1cefc",
+    "url": "js/Play.8d8eca85.js"
+  },
+  {
+    "revision": "6ce92972b00ef2c6a3f0",
+    "url": "js/PopularGame.be730dee.js"
+  },
+  {
+    "revision": "e4e09b760af23de7f933",
+    "url": "js/ReportReview.d82f9a7e.js"
+  },
+  {
+    "revision": "1d5b307ba96f285fa661",
+    "url": "js/Room.f3a0325a.js"
+  },
+  {
+    "revision": "6c79b098ec7ce5c07363",
+    "url": "js/Start.903cee4a.js"
+  },
+  {
+    "revision": "ed0bed771ac94382927d",
+    "url": "js/UserCenter.b248cfcd.js"
+  },
+  {
+    "revision": "2095d1bc6c742c54e6d3",
+    "url": "js/app.3f7131fb.js"
+  },
+  {
+    "revision": "761270562d6c4e82597b",
+    "url": "js/chunk-vendors.da245246.js"
+  },
+  {
+    "revision": "c78f2d8fa0a89267e7f9883898f0baff",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
